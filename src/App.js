b170d0c13f680d06a5aa1f221a1d3be96ab74dad
RidwanSiddique44 <srsiddique118@gmail.com>
2022-05-23 20:35:25 +0600

    signup added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/MainPages/Home/Home';
 import NotFound from './Components/MainPages/NotFound/NotFound';
+import SignUp from './Components/RegistrationPages/SignUp/SignUp';
 import Footer from './Components/SharedPages/Footer/Footer';
 import Header from './Components/SharedPages/Header/Header';
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
 
---
+++ src/Components/RegistrationPages/SignUp/SignUp.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import auth from '../../../firebase.init';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+
+const SignUp = () => {
+    const navigate = useNavigate();
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const [createUserWithEmailAndPassword, user, loading, error,]
+        = useCreateUserWithEmailAndPassword(auth);
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const name = e.target.fullname.value;
+        createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name });
+        navigate('/home');
+    }
+
+    return (
+        <div>
+            <div className="bg-grey-lighter min-h-screen flex flex-col">
+                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                    <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                        <h1 className="mb-8 text-3xl  text-center">Sign up</h1>
+                        <form onSubmit={handleSignup}>
+                            <input
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="fullname"
+                                placeholder="Full Name" />
+
+                            <input
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="email"
+                                placeholder="Email" />
+
+                            <input
+                                type="password"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="password"
+                                placeholder="Password" />
+
+                            <button
+                                type="submit"
+                                className="w-full text-center py-3 rounded bg-gray-700 text-white hover:bg-gray-900 focus:outline-none my-1"
+                            >Create Account</button>
+                        </form>
+                        <div className="text-grey-dark mt-6">
+                            Already have an account?
+                            <a className="no-underline border-b border-blue text-blue-900" href="../login/">Log in </a>.
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
